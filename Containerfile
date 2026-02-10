cc43be30e7070cd13bf9eb97a7d41c4beec3fa42
John Mertz <git@john.me.tz>
2026-02-09 22:34:52 -0700

    Port to podman and add ARCH and DISTRO arguments
    Ideally I'd like for it to be able to build a matrix of stable and
    testing, with at least amd64 and arm64.

--- Containerfile
+++ Containerfile
@@ -1,6 +1,10 @@
-FROM debian:trixie
-ARG EXIM_VERSION=${EXIM_VERSION}
-ARG CPUS=${CPUS}
+ARG DISTRO=trixie
+FROM debian:${DISTRO}
+
+ARG EXIM_VERSION
+ARG ARCH
+ARG CPUS
+
 COPY ./DEBIAN /DEBIAN
 RUN mkdir -p /st-exim/opt/exim4
 
@@ -8,6 +12,7 @@ RUN apt-get update
 RUN DEBIAN_FRONTEND=noninteractive apt-get --assume-yes install \
     apt-utils \
     build-essential \
+    ca-certificates \
     exim4-daemon-heavy \
     git \
     libgcrypt20-dev \
@@ -27,23 +32,33 @@ RUN DEBIAN_FRONTEND=noninteractive apt-get --assume-yes install \
     libspf2-dev \
     libsqlite3-dev \
     lintian \
-    pkg-config
-RUN useradd spamtagger --system --create-home --user-group --home-dir /var/spamtagger --shell /usr/sbin/nologin
+    pkg-config && \
+  rm -rf /var/lib/apt/lists/*
+
+RUN useradd spamtagger --system --create-home --user-group \
+  --home-dir /var/spamtagger \
+  --shell /usr/sbin/nologin
+
 RUN git clone https://github.com/exim/exim.git --depth 1
 WORKDIR exim
-RUN git fetch --tags
-RUN git checkout exim-${EXIM_VERSION}
-RUN ldconfig
-WORKDIR src
+RUN git fetch --tags && git checkout exim-${EXIM_VERSION}
+
+WORKDIR /exim/src
 RUN mkdir Local
 RUN cp /DEBIAN/EDITME Local/Makefile
 RUN EXIM_RELEASE_VERSION=${EXIM_VERSION} make -j${CPUS}
 RUN make install
+
+RUN mkdir -p /st-exim/opt/exim4
 RUN cp -a /opt/exim4/* /st-exim/opt/exim4/
 RUN cp -r /DEBIAN /st-exim/
+
+RUN sed -i 's/__INSTVER__/'$EXIM_VERSION'/' /st-exim/DEBIAN/control && \
+  sed -i 's/__INSTSIZE__/'$(du -sk /st-exim | cut -f1)'/' /st-exim/DEBIAN/control
+
 WORKDIR /
-RUN sed -i 's/__INSTVER__/'$EXIM_VERSION'/' /st-exim/DEBIAN/control
-RUN sed -i 's/__INSTSIZE__/'$(du -sk /st-exim | cut -f1)'/' /st-exim/DEBIAN/control
-WORKDIR /
-RUN dpkg-deb -b -Z gzip /st-exim /st-exim-${EXIM_VERSION}_amd64.deb
-CMD lintian /st-exim-${EXIM_VERSION}_amd64.deb
+
+RUN dpkg-deb -b -Z gzip /st-exim /tmp/st-exim.deb && \
+  lintian /tmp/st-exim.deb || true
+
+CMD cp /tmp/st-exim.deb /out/
--- build_and_extract.sh
+++ build_and_extract.sh
@@ -1,50 +1,36 @@
-#!/bin/bash
+#!/usr/bin/env bash
+# vim: set ts=2 sw=2 expandtab :
+set -euo pipefail
 
-if [ -z $1 ]; then
-	EXIM_VERSION="4.99"
-	echo "Version argument not provided. Defaulting to $EXIM_VERSION"
+EXIM_VERSION="${1:-4.99}"
+DISTRO="${2:-trixie}"
+ARCH="${3:-amd64}"
+EXPORT_DIR="${4:-$(pwd)/dist}"
+FINAL_DEB="st-exim-${EXIM_VERSION}_${ARCH}.deb"
+IMAGE="st-exim:${EXIM_VERSION}-${DISTRO}-${ARCH}"
+CPUS="$(nproc)"
+
+echo "Building Exim $EXIM_VERSION for ${DISTRO}/${ARCH}"
+mkdir -p "${EXPORT_DIR}"
+
+podman build \
+  --build-arg EXIM_VERSION=$EXIM_VERSION \
+  --build-arg DISTRO=$DISTRO \
+  --build-arg ARCH=$ARCH \
+  --build-arg CPUS=$CPUS \
+  -t "$IMAGE" .
+
+podman run --rm \
+  --arch "${ARCH}" \
+  -v "${EXPORT_DIR}:/out:Z" \
+  "localhost/${IMAGE}"
+
+if [[ -f "${EXPORT_DIR}/st-exim.deb" ]]; then
+  echo "✔ Successfully built package"
 else
-	EXIM_VERSION=$1
-fi
-echo "Building version $EXIM_VERSION"
-
-if [ -z $2 ]; then
-	EXPORT_DIR=${HOME}/debs
-else
-	EXPORT_DIR=$2
+  echo "✘ Failed to build package"
+  exit 1
 fi
 
-if [ ! -d $EXPORT_DIR ]; then
-	mkdir -p $EXPORT_DIR
-fi
-
-CPUS=$(lscpu | grep '^CPU(s):' | awk '{ print $2 }');
-docker build --build-arg EXIM_VERSION=$EXIM_VERSION --build-arg CPUS=$CPUS -t build/st-exim .
-
-docker image inspect build/st-exim >/dev/null 2>&1
-RET=$?
-if [ $RET != 0 ]; then
-	echo "Failed to build image with 'docker build --build-arg EXIM_VERSION=$EXIM_VERSION --build-arg CPUS=$CPUS -t build/st-exim'. Return code $RET"
-	exit
-else
-	echo "Built docker image: build/st-exim"
-fi
-
-CONTAINER=$(docker run -d build/st-exim)
-RET=$?
-if [ $RET != 0 ]; then
-	echo "Failed to run container ($CONTAINER) with 'docker run -d build/st-exim' Return code $RET"
-	exit
-fi
-
-docker cp $CONTAINER:/st-exim-${EXIM_VERSION}_amd64.deb ${EXPORT_DIR}/
-
-echo Cleaning up...
-sleep 5
-docker rm $CONTAINER >/dev/null
-docker image rm build/st-exim -f >/dev/null
-if [ -f ${EXPORT_DIR}/st-exim-${EXIM_VERSION}_amd64.deb ]; then
-	echo "Package build and exported to: ${EXPORT_DIR}/st-exim-${EXIM_VERSION}_amd64.deb"
-else
-	echo "Failed to export package st-exim-${EXIM_VERSION}_amd64.deb to ${EXPORT_DIR}/st-exim-${EXIM_VERSION}_amd64.deb"
-fi
+mv ${EXPORT_DIR}/st-exim.deb ${EXPORT_DIR}/${FINAL_DEB}
+echo "Package available at ${EXPORT_DIR}/${FINAL_DEB}"
